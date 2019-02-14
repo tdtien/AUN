8299f3b08003653e463f424d114b9cb0581e62e7
Tran Duy Tien <tdtien.it@gmail.com>
2019-02-14 22:29:07 +0700

    Update Login view

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,50 +11,100 @@ import {
   ListItem,
   Body,
   View,
-  Header
+  Toast,
+  Root,
+  Footer
 } from "native-base";
 import { StyleSheet, Image } from "react-native";
-import {Actions} from 'react-native-router-flux'
+import { Actions } from "react-native-router-flux";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: "",
+      password: "",
+      isLoading: false
+    };
   }
+
+  handleLogin = () => {
+    this.setState({ isLoading: true });
+    console.log(this.state.email);
+    console.log(this.state.password);
+    if (this.state.email === "") {
+      Toast.show({
+        text: "Please enter your email address",
+        type: "warning",
+        buttonText: "Okay"
+      });
+    } else if (this.state.password === "") {
+      Toast.show({
+        text: "Please enter your password",
+        type: "warning",
+        buttonText: "Okay"
+      });
+    } else {
+      Toast.show({
+        text: "Login successful",
+        type: "success",
+        buttonText: "Okay"
+      });
+    }
+  };
 
   render() {
     return (
-      <Container>
-        <Header androidStatusBarColor="#fff" style={{backgroundColor:"#fff"}}/>
-        <Content>
-          <View style={styles.logoContainer}>
-            <Image
-              style={styles.logo}
-              source={require("../../assets/images/logo.jpg")}
-            />
-            <Text style={styles.title}>Food Delivery Now</Text>
-          </View>
+      <Root>
+        <Container>
+          <Content>
+            <View style={styles.logoContainer}>
+              <Image
+                style={styles.logo}
+                source={require("../../assets/images/logo.jpg")}
+              />
+              <Text style={styles.title}>AUN Inspection System</Text>
+            </View>
+          </Content>
           <Form>
             <Item stackedLabel>
-              <Label>Username</Label>
-              <Input />
+              <Label>Email</Label>
+              <Input
+                keyboardType="email-address"
+                onChangeText={email => this.setState({ email: email })}
+                onSubmitEditing={() => this.refs["passwordInput"]._root.focus()}
+                returnKeyType="next"
+                autoCapitalize="none"
+              />
             </Item>
             <Item stackedLabel last>
               <Label>Password</Label>
-              <Input />
+              <Input
+                autoCapitalize="none"
+                secureTextEntry
+                onChangeText={password => this.setState({ password: password })}
+                ref="passwordInput"
+                returnKeyType="go"
+                onSubmitEditing={() => this.handleLogin()}
+              />
             </Item>
             <ListItem noBorder>
               <Body>
-                <Button info block>
+                <Button info block onPress={() => this.handleLogin()}>
                   <Text>Sign In</Text>
                 </Button>
-                <Button info block style={styles.button} onPress = {() => Actions.register()}>
+              </Body>
+            </ListItem>
+            <ListItem noBorder>
+              <Body>
+                <Button info block onPress={() => Actions.register()}>
                   <Text>Register</Text>
                 </Button>
               </Body>
             </ListItem>
           </Form>
-        </Content>
-      </Container>
+        </Container>
+      </Root>
     );
   }
 }
@@ -70,7 +120,7 @@ const styles = StyleSheet.create({
     height: 100
   },
   title: {
-    color: "#FFF",
+    color: "#424242",
     marginTop: 10,
     fontSize: 20,
     textAlign: "center",
