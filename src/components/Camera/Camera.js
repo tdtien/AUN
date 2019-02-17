29f5ee7a139420793f7a5cc8ba2e85758364c5e3
manhtu1997 <manhtu.1997@gmail.com>
2019-02-17 13:04:29 +0700

    Update Camera feature

--- src/components/Camera/Camera.js
+++ src/components/Camera/Camera.js
@@ -1,7 +1,10 @@
 import { Button, Container, Icon, Text, View } from "native-base";
 import React, { Component } from "react";
-import { StatusBar, StyleSheet } from "react-native";
+import { StatusBar, StyleSheet, Modal, Image } from "react-native";
 import { RNCamera } from "react-native-camera";
+import { Actions } from "react-native-router-flux";
+import ImageViewer from 'react-native-image-zoom-viewer';
+import Images from "../../assets/images";
 
 const PendingView = () => (
   <Container
@@ -15,56 +18,92 @@ const PendingView = () => (
 );
 
 export default class Camera extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flashIcon: 'off',
+      flashMode: RNCamera.Constants.FlashMode.off,
+      imageUrl: [],
+      imageDisplay: false,
+    }
+    // this.state.imageUrl.push({props: {source: Images.logo}})
+  }
+
   render() {
     return (
-      <Container>
+      <View style={styles.container}>
         <StatusBar hidden />
-        <RNCamera
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          permissionDialogTitle={"Permission to use camera"}
-          permissionDialogMessage={
-            "We need your permission to use your camera phone"
-          }
-          captureAudio={false}
-        >
-          {({ camera, status }) => {
-            if (status !== "READY") return <PendingView />;
-            return (
-              <View
-                style={{
-                  flex: 0,
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  margin: 15
-                }}
-              >
-                <Button
-                  onPress={() => this.takePicture(camera)}
-                  icon
-                  transparent
-                >
-                  <Icon
-                    style={{ fontSize: 40, color: "#fff" }}
-                    name="camera"
-                    type="FontAwesome5"
-                  />
-                </Button>
-              </View>
-            );
-          }}
-        </RNCamera>
-      </Container>
+        {this.state.imageDisplay ? <Modal visible={this.state.imageDisplay} transparent={true}>
+          <ImageViewer imageUrls={this.state.imageUrl} enableSwipeDown onCancel={() => this.setState({imageDisplay: false})} />
+        </Modal> :
+          <RNCamera
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={this.state.flashMode}
+            permissionDialogTitle={"Permission to use camera"}
+            permissionDialogMessage={
+              "We need your permission to use your camera phone"
+            }
+            captureAudio={false}
+          >
+            {({ camera, status }) => {
+              if (status !== "READY") return <PendingView />;
+              return (
+                <View style={{ flex: 1, justifyContent: "space-between" }}>
+                  <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                    <Button transparent onPress={() => Actions.pop()}>
+                      <Icon name='ios-arrow-back' style={{ color: "#fff", fontSize: 30 }} />
+                    </Button>
+                    <Button transparent onPress={() => this.changeFlashMode()}>
+                      <Icon name={'flash-' + this.state.flashIcon} style={{ color: "#fff", fontSize: 30 }} type="MaterialIcons" />
+                    </Button>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Button
+                      onPress={() => this.takePicture(camera)}
+                      icon
+                      transparent
+                      style={{ margin: 15 }}
+                    >
+                      <Icon
+                        style={{ fontSize: 40, color: "#fff" }}
+                        name="camera"
+                        type="FontAwesome5"
+                      />
+                    </Button>
+                  </View>
+                </View>
+              );
+            }}
+          </RNCamera>
+        }
+      </View>
     );
   }
 
-  takePicture = async function(camera) {
+  takePicture = async function (camera) {
     const options = { quality: 0.5, base64: true };
     const data = await camera.takePictureAsync(options);
-    //  eslint-disable-next-line
-    console.log(data.uri);
+    this.state.imageUrl.unshift({url: data.uri})
+    this.setState({ imageDisplay: true })
   };
+
+  changeFlashMode = () => {
+    if (this.state.flashIcon === 'on') {
+      this.setState({ flashIcon: 'auto', flashMode: RNCamera.Constants.FlashMode.auto })
+    }
+    if (this.state.flashIcon === 'auto') {
+      this.setState({ flashIcon: 'off', flashMode: RNCamera.Constants.FlashMode.off })
+    }
+    if (this.state.flashIcon === 'off') {
+      this.setState({ flashIcon: 'on', flashMode: RNCamera.Constants.FlashMode.on })
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -75,7 +114,6 @@ const styles = StyleSheet.create({
   },
   preview: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center"
+    flexDirection: "column",
   },
 });
