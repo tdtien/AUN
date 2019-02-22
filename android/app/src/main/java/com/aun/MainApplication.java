3e4858fc4fa7412d50fc2e77670b5f4c2bc85188
Tien Tran <tdtien.it@gmail.com>
2019-02-22 23:09:59 +0700

    Add lib react-native-image-crop-picker

--- android/app/src/main/java/com/aun/MainApplication.java
+++ android/app/src/main/java/com/aun/MainApplication.java
@@ -3,6 +3,7 @@ package com.aun;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.anyline.RNImageToPDF.RNImageToPdfPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PickerPackage(),
             new VectorIconsPackage(),
             new RNImageToPdfPackage(),
             new RNCameraPackage()
